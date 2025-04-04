e762730ff9423eeceb53f5d41b8e5ceee74668d1
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-04 12:55:19 +0400

    add mapping profiles

--- GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
+++ GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
-using GloboTicket.TicketManagement.Application.Features.Events;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
 using GloboTicket.TicketManagement.Domain.Entities;
 
 namespace GloboTicket.TicketManagement.Application.Profiles
@@ -11,6 +14,9 @@ namespace GloboTicket.TicketManagement.Application.Profiles
             CreateMap<Event, EventListVm>().ReverseMap();
             CreateMap<Event, EventDetailVm>().ReverseMap();
             CreateMap<Category, CategoryDto>().ReverseMap();
+
+            CreateMap<Category, CategoryListVm>().ReverseMap();
+            CreateMap<Category, CategoryEventListVm>().ReverseMap();
         }
     }
 }
